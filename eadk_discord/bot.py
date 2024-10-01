5d24b4621cff93dad9700210c60a653be00c9ad1
Albert Garde <albertsgarde@gmail.com>
2024-10-01 22:10:20 +0200

    :recycle: Separate command logic from IO

---
+++ eadk_discord/bot.py
@@ -0,0 +1,224 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Callable
+from zoneinfo import ZoneInfo
+
+import discord
+from discord.app_commands import AppCommandError
+from pydantic import BaseModel, Field
+
+from eadk_discord import dates, fmt
+from eadk_discord.database import Database
+from eadk_discord.event import BookDesk, Event, MakeFlex, MakeOwned, UnbookDesk
+from eadk_discord.state import HandleEventError
+
+TIME_ZONE = ZoneInfo("Europe/Copenhagen")
+
+
+class CommandInfo(BaseModel):
+    now: datetime = Field()
+    format_user: Callable[[int], str] = Field()
+    author_id: int = Field()
+
+    @staticmethod
+    def from_interaction(interaction: discord.Interaction) -> "CommandInfo":
+        return CommandInfo(
+            now=datetime.now(TIME_ZONE),
+            format_user=lambda user: fmt.user(interaction, user),
+            author_id=interaction.user.id,
+        )
+
+
+@dataclass
+class Response:
+    message: str
+    ephemeral: bool
+    embed: discord.Embed | None
+
+    def __init__(self, message: str = "", ephemeral: bool = False, embed: discord.Embed | None = None) -> None:
+        self.message = message
+        self.ephemeral = ephemeral
+        self.embed = embed
+
+    async def send(self, interaction: discord.Interaction) -> None:
+        if self.embed is None:
+            await interaction.response.send_message(self.message, ephemeral=self.ephemeral)
+        else:
+            await interaction.response.send_message(self.message, ephemeral=self.ephemeral, embed=self.embed)
+
+
+class EADKBot:
+    _database: Database
+
+    def __init__(self, database: Database) -> None:
+        self._database = database
+
+    def info(self, info: CommandInfo, date_str: str | None) -> Response:
+        booking_date = dates.get_booking_date(date_str, info.now)
+        booking_day, _ = self._database.state.day(booking_date)
+
+        desk_numbers_str = "\n".join(str(i + 1) for i in range(len(booking_day.desks)))
+        desk_bookers_str = "\n".join(
+            info.format_user(desk.booker) if desk.booker else "**Free**" for desk in booking_day.desks
+        )
+        desk_owners_str = "\n".join(
+            info.format_user(desk.owner) if desk.owner else "**Flex**" for desk in booking_day.desks
+        )
+
+        return Response(
+            message="",
+            ephemeral=True,
+            embed=discord.Embed(title="Desk availability", description=f"{booking_date.strftime('%A %Y-%m-%d')}")
+            .add_field(name="Desk", value=desk_numbers_str, inline=True)
+            .add_field(name="Booked by", value=desk_bookers_str, inline=True)
+            .add_field(name="Owner", value=desk_owners_str, inline=True),
+        )
+
+    def book(self, info: CommandInfo, date_str: str | None, user_id: int | None, desk_arg: int | None) -> Response:
+        if user_id is None:
+            user_id = info.author_id
+
+        booking_date = dates.get_booking_date(date_str, info.now)
+        booking_day, _ = self._database.state.day(booking_date)
+        date_str = fmt.date(booking_date)
+
+        if desk_arg:
+            if desk_arg < 1 or desk_arg > len(booking_day.desks):
+                return Response(
+                    message=f"Desk {desk_arg} does not exist. There are only {len(booking_day.desks)} desks.",
+                    ephemeral=True,
+                )
+            desk_index = desk_arg - 1
+            desk_num = desk_arg
+        else:
+            desk_index_option = booking_day.get_available_desk()
+            if desk_index_option is not None:
+                desk_index = desk_index_option
+                desk_num = desk_index + 1
+            else:
+                return Response(message=f"No more desks are available for booking on {date_str}.", ephemeral=True)
+        self._database.handle_event(
+            Event(
+                author=info.author_id,
+                time=datetime.now(),
+                event=BookDesk(date=booking_date, desk_index=desk_index, user=user_id),
+            )
+        )
+        return Response(message=f"Desk {desk_num} has been booked for {info.format_user(user_id)} on {date_str}.")
+
+    def unbook(self, info: CommandInfo, date_str: str | None, user_id: int | None, desk: int | None) -> Response:
+        booking_date = dates.get_booking_date(date_str, info.now)
+        booking_day, _ = self._database.state.day(booking_date)
+        date_str = fmt.date(booking_date)
+
+        if booking_date < info.now.date():
+            return Response(
+                message=f"Date {date_str} not available for booking. Desks cannot be unbooked in the past.",
+                ephemeral=True,
+            )
+
+        if desk is not None:
+            if desk < 1 or desk > len(booking_day.desks):
+                return Response(
+                    message=f"Desk {desk} does not exist. There are only {len(booking_day.desks)} desks.",
+                    ephemeral=True,
+                )
+            desk_index = desk - 1
+            desk_num = desk
+            if user_id is not None:
+                if user_id != booking_day.desk(desk_index).booker:
+                    return Response(
+                        message=f"Desk {desk} is not booked by {info.format_user(user_id)} on {date_str}.",
+                        ephemeral=True,
+                    )
+        else:
+            if user_id is None:
+                user_id = info.author_id
+            desk_indices = booking_day.booked_desks(user_id)
+            if desk_indices:
+                desk_index = desk_indices[0]
+                desk_num = desk_index + 1
+            else:
+                return Response(
+                    message=f"{info.format_user(user_id)} already has no desks booked for {date_str}.", ephemeral=True
+                )
+
+        desk_booker = booking_day.desk(desk_index).booker
+        if desk_booker:
+            self._database.handle_event(
+                Event(
+                    author=info.author_id,
+                    time=datetime.now(),
+                    event=UnbookDesk(date=booking_date, desk_index=desk_index),
+                )
+            )
+            return Response(
+                message=f"Desk {desk_num} is no longer booked for {info.format_user(desk_booker)} on {date_str}."
+            )
+        else:
+            return Response(message=f"Desk {desk_num} is already free on {date_str}.", ephemeral=True)
+
+    def makeowned(self, info: CommandInfo, start_date_str: str, user_id: int | None, desk_num: int) -> Response:
+        booking_date = dates.get_booking_date(start_date_str, info.now)
+        date_str = fmt.date(booking_date)
+
+        if booking_date < info.now.date():
+            return Response(
+                message=f"Date {date_str} not available for booking. Desks cannot be made permanent retroactively.",
+                ephemeral=True,
+            )
+
+        desk_index = desk_num - 1
+
+        if user_id is None:
+            user_id = info.author_id
+        self._database.handle_event(
+            Event(
+                author=info.author_id,
+                time=datetime.now(),
+                event=MakeOwned(start_date=booking_date, desk_index=desk_index, user=user_id),
+            )
+        )
+        return Response(message=f"Desk {desk_num} is now owned by {info.format_user(user_id)} from {date_str} onwards.")
+
+    def makeflex(self, info: CommandInfo, start_date_str: str, desk_num: int) -> Response:
+        booking_date = dates.get_booking_date(start_date_str, info.now)
+        date_str = fmt.date(booking_date)
+
+        if booking_date < info.now.date():
+            return Response(
+                message=f"Date {date_str} not available for booking. You cannot make a desk permanent retroactively.",
+                ephemeral=True,
+            )
+
+        desk_index = desk_num - 1
+
+        self._database.handle_event(
+            Event(
+                author=info.author_id,
+                time=datetime.now(),
+                event=MakeFlex(start_date=booking_date, desk_index=desk_index),
+            )
+        )
+        return Response(message=f"Desk {desk_num} is now a flex desk from {date_str} onwards.")
+
+    def handle_error(self, info: CommandInfo, error: AppCommandError) -> Response:
+        if isinstance(error, discord.app_commands.errors.MissingAnyRole) or isinstance(
+            error, discord.app_commands.errors.MissingRole
+        ):
+            return Response(message="You do not have permission to run this command.", ephemeral=True)
+        if isinstance(error, discord.app_commands.errors.CheckFailure):
+            return Response(message="This command can only be used in the office channel.", ephemeral=True)
+        if isinstance(error, discord.app_commands.errors.TransformerError):
+            match error.__cause__:
+                case dates.DateParseError(arg):
+                    return Response(
+                        message=f"Date {arg} could not be parsed. "
+                        "Please use the format YYYY-MM-DD, 'today', 'tomorrow', or specify a weekday.",
+                        ephemeral=True,
+                    )
+        if isinstance(error, discord.app_commands.errors.CommandInvokeError):
+            match error.__cause__:
+                case HandleEventError(_event, event_error):
+                    return Response(message=event_error.message(info.format_user), ephemeral=True)
+        raise error
--- eadk_discord/bot_setup.py
+++ eadk_discord/bot_setup.py
@@ -1,43 +1,21 @@
 import logging
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 from pathlib import Path
-from zoneinfo import ZoneInfo
 
 import discord
 from discord import Intents, Interaction, Member, app_commands
 from discord.abc import Snowflake
-from discord.app_commands import AppCommandError, Choice, Range, Transform
+from discord.app_commands import AppCommandError, Choice, Range
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
 
-from eadk_discord import fmt
+from eadk_discord.bot import CommandInfo, EADKBot, Response
 from eadk_discord.database import Database
-from eadk_discord.date_converter import DateConverter, DateParseError
-from eadk_discord.event import BookDesk, Event, MakeFlex, MakeOwned, SetNumDesks, UnbookDesk
-from eadk_discord.state import HandleEventError
-
-TIME_ZONE = ZoneInfo("Europe/Copenhagen")
+from eadk_discord.event import Event, SetNumDesks
 
 TEST_SERVER_ROLE_ID = 1287776907563106436
 EADK_DESK_ADMIN_ID = 1288070128533114880
 EADK_DESK_REGULAR_ID = 1288068945324146718
-
-
-def format_date(date: date) -> str:
-    return date.isoformat()
-
-
-def get_booking_date(booking_date_arg: date | None) -> date:
-    """
-    Returns a tuple of two values:
-    - A boolean indicating whether it is currently before 17:00.
-    - A date object representing the date on which desks should be booked.
-    """
-    if booking_date_arg is not None:
-        return booking_date_arg
-    now = datetime.now(TIME_ZONE)
-    booking_date = now.date() if now.hour < 17 else now.date() + timedelta(days=1)
-    return booking_date
 
 
 def author_id(interaction: Interaction) -> int:
@@ -69,245 +47,84 @@ def setup_bot(database_path: Path, guilds: list[Snowflake]) -> Bot:
     intents.message_content = True
     intents.members = True
 
+    eadk_bot = EADKBot(database)
     bot = Bot(command_prefix="!", intents=intents)
 
     @bot.tree.command(name="info", description="Get current booking status.", guilds=guilds)
-    @app_commands.autocomplete(booking_date_arg=date_autocomplete)
-    @app_commands.rename(booking_date_arg="date")
+    @app_commands.autocomplete(date_arg=date_autocomplete)
+    @app_commands.rename(date_arg="date")
     @app_commands.check(channel_check)
     async def info(
         interaction: Interaction,
-        booking_date_arg: Transform[date, DateConverter] | None,
+        date_arg: str | None,
     ) -> None:
-        booking_date = get_booking_date(booking_date_arg)
-        booking_day, _ = database.state.day(booking_date)
-
-        desk_numbers_str = "\n".join(str(i + 1) for i in range(len(booking_day.desks)))
-        desk_bookers_str = "\n".join(
-            fmt.user(interaction, desk.booker) if desk.booker else "**Free**" for desk in booking_day.desks
-        )
-        desk_owners_str = "\n".join(
-            fmt.user(interaction, desk.owner) if desk.owner else "**Flex**" for desk in booking_day.desks
-        )
-
-        await interaction.response.send_message(
-            embed=discord.Embed(title="Desk availability", description=f"{booking_date.strftime('%A %Y-%m-%d')}")
-            .add_field(name="Desk", value=desk_numbers_str, inline=True)
-            .add_field(name="Booked by", value=desk_bookers_str, inline=True)
-            .add_field(name="Owner", value=desk_owners_str, inline=True),
-            ephemeral=True,
-        )
+        await eadk_bot.info(
+            CommandInfo.from_interaction(interaction),
+            date_arg,
+        ).send(interaction)
 
     @bot.tree.command(name="book", description="Book a desk.", guilds=guilds)
     @app_commands.autocomplete(booking_date_arg=date_autocomplete)
-    @app_commands.rename(booking_date_arg="date")
-    @app_commands.rename(desk="desk_id")
+    @app_commands.rename(booking_date_arg="date", desk="desk_id")
     @app_commands.check(channel_check)
     @app_commands.checks.has_any_role(TEST_SERVER_ROLE_ID, EADK_DESK_ADMIN_ID, EADK_DESK_REGULAR_ID)
     async def book(
         interaction: Interaction,
-        booking_date_arg: Transform[date, DateConverter] | None,
+        booking_date_arg: str | None,
         user: Member | None,
         desk: Range[int, 1] | None,
     ) -> None:
-        booking_date = get_booking_date(booking_date_arg)
-        booking_day, _ = database.state.day(booking_date)
-        date_str = format_date(booking_date)
-
-        if booking_date < date.today():
-            await interaction.response.send_message(
-                f"Date {date_str} not available for booking. Desks cannot be booked in the past.", ephemeral=True
-            )
-            return
-
-        if user:
-            user_id = user.id
-        else:
-            user_id = author_id(interaction)
-
-        if desk:
-            if desk < 1 or desk > len(booking_day.desks):
-                await interaction.response.send_message(
-                    f"Desk {desk} does not exist. There are only {len(booking_day.desks)} desks.", ephemeral=True
-                )
-                return
-            desk_index = desk - 1
-            desk_num = desk
-        else:
-            desk_index_option = booking_day.get_available_desk()
-            if desk_index_option is not None:
-                desk_index = desk_index_option
-                desk_num = desk_index + 1
-            else:
-                await interaction.response.send_message(
-                    f"No more desks are available for booking on {date_str}.", ephemeral=True
-                )
-                return
-        database.handle_event(
-            Event(
-                author=author_id(interaction),
-                time=datetime.now(),
-                event=BookDesk(date=booking_date, desk_index=desk_index, user=user_id),
-            )
-        )
-        await interaction.response.send_message(
-            f"Desk {desk_num} has been booked for {fmt.user(interaction, user_id)} on {date_str}."
-        )
+        await eadk_bot.book(
+            CommandInfo.from_interaction(interaction),
+            booking_date_arg,
+            user.id if user else None,
+            desk,
+        ).send(interaction)
         database.save(database_path)
 
     @bot.tree.command(name="unbook", description="Unbook a desk.", guilds=guilds)
     @app_commands.autocomplete(booking_date_arg=date_autocomplete)
-    @app_commands.rename(booking_date_arg="date")
-    @app_commands.rename(desk="desk_id")
+    @app_commands.rename(booking_date_arg="date", desk="desk_id")
     @app_commands.check(channel_check)
     @app_commands.checks.has_any_role(TEST_SERVER_ROLE_ID, EADK_DESK_ADMIN_ID, EADK_DESK_REGULAR_ID)
     async def unbook(
         interaction: Interaction,
-        booking_date_arg: Transform[date, DateConverter] | None,
+        booking_date_arg: str | None,
         user: Member | None,
         desk: Range[int, 1] | None,
     ) -> None:
-        booking_date = get_booking_date(booking_date_arg)
-        booking_day, _ = database.state.day(booking_date)
-        date_str = format_date(booking_date)
-
-        if booking_date < date.today():
-            await interaction.response.send_message(
-                f"Date {date_str} not available for booking. Desks cannot be unbooked in the past.", ephemeral=True
-            )
-            return
-
-        if desk is not None:
-            if desk < 1 or desk > len(booking_day.desks):
-                await interaction.response.send_message(
-                    f"Desk {desk} does not exist. There are only {len(booking_day.desks)} desks.", ephemeral=True
-                )
-                return
-            desk_index = desk - 1
-            desk_num = desk
-            if user is not None:
-                if user.id != booking_day.desk(desk_index).booker:
-                    await interaction.response.send_message(
-                        f"Desk {desk} is not booked by {fmt.user(interaction, user.id)} on {date_str}.",
-                        ephemeral=True,
-                    )
-                    return
-        else:
-            if user:
-                user_id = user.id
-            else:
-                user_id = author_id(interaction)
-            desk_indices = booking_day.booked_desks(user_id)
-            if desk_indices:
-                desk_index = desk_indices[0]
-                desk_num = desk_index + 1
-            else:
-                await interaction.response.send_message(
-                    f"{fmt.user(interaction, user_id)} already has no desks booked for {date_str}.", ephemeral=True
-                )
-                return
-
-        desk_booker = booking_day.desk(desk_index).booker
-        if desk_booker:
-            database.handle_event(
-                Event(
-                    author=author_id(interaction),
-                    time=datetime.now(),
-                    event=UnbookDesk(date=booking_date, desk_index=desk_index),
-                )
-            )
-            await interaction.response.send_message(
-                f"Desk {desk_num} is no longer booked for {fmt.user(interaction, desk_booker)} on {date_str}."
-            )
-        else:
-            await interaction.response.send_message(f"Desk {desk_num} is already free on {date_str}.", ephemeral=True)
+        await eadk_bot.unbook(
+            CommandInfo.from_interaction(interaction), booking_date_arg, user.id if user else None, desk
+        ).send(interaction)
         database.save(database_path)
 
     @bot.tree.command(
         name="makeowned", description="Make a user the owner of the desk from a specific date onwards", guilds=guilds
     )
-    @app_commands.autocomplete(start_date=date_autocomplete)
-    @app_commands.rename(desk="desk_id")
+    @app_commands.autocomplete(start_date_str=date_autocomplete)
+    @app_commands.rename(start_date_str="start_date", desk="desk_id")
     @app_commands.check(channel_check)
     @app_commands.checks.has_any_role(TEST_SERVER_ROLE_ID, EADK_DESK_ADMIN_ID)
     async def makeowned(
         interaction: Interaction,
-        start_date: Transform[date, DateConverter],
+        start_date_str: str,
         user: Member | None,
         desk: Range[int, 1],
     ) -> None:
-        booking_date = get_booking_date(start_date)
-        booking_day, _ = database.state.day(booking_date)
-        date_str = format_date(booking_date)
-
-        if booking_date < date.today():
-            await interaction.response.send_message(
-                f"Date {date_str} not available for booking. Desks cannot be made permanent retroactively.",
-                ephemeral=True,
-            )
-            return
-
-        if desk < 1 or desk > len(booking_day.desks):
-            await interaction.response.send_message(
-                f"Desk {desk} does not exist. There are only {len(booking_day.desks)} desks.", ephemeral=True
-            )
-            return
-
-        desk_index = desk - 1
-
-        if user:
-            user_id = user.id
-        else:
-            user_id = author_id(interaction)
-        database.handle_event(
-            Event(
-                author=author_id(interaction),
-                time=datetime.now(),
-                event=MakeOwned(start_date=booking_date, desk_index=desk_index, user=user_id),
-            )
-        )
-        await interaction.response.send_message(
-            f"Desk {desk} is now owned by {fmt.user(interaction, user_id)} from {date_str} onwards."
-        )
+        await eadk_bot.makeowned(
+            CommandInfo.from_interaction(interaction), start_date_str, user.id if user else None, desk
+        ).send(interaction)
         database.save(database_path)
 
     @bot.tree.command(
         name="makeflex", description="Make a desk a flex desk from a specific date onwards", guilds=guilds
     )
-    @app_commands.autocomplete(start_date=date_autocomplete)
-    @app_commands.rename(desk="desk_id")
+    @app_commands.autocomplete(start_date_str=date_autocomplete)
+    @app_commands.rename(start_date_str="start_date", desk="desk_id")
     @app_commands.check(channel_check)
     @app_commands.checks.has_any_role(TEST_SERVER_ROLE_ID, EADK_DESK_ADMIN_ID)
-    async def makeflex(
-        interaction: Interaction, start_date: Transform[date, DateConverter], desk: Range[int, 1]
-    ) -> None:
-        booking_date = get_booking_date(start_date)
-        booking_day, _ = database.state.day(booking_date)
-        date_str = format_date(booking_date)
-
-        if booking_date < date.today():
-            await interaction.response.send_message(
-                f"Date {date_str} not available for booking. You cannot make a desk permanent retroactively.",
-                ephemeral=True,
-            )
-            return
-
-        if desk < 1 or desk > len(booking_day.desks):
-            await interaction.response.send_message(
-                f"Desk {desk} does not exist. There are only {len(booking_day.desks)} desks.", ephemeral=True
-            )
-            return
-
-        desk_index = desk - 1
-
-        database.handle_event(
-            Event(
-                author=author_id(interaction),
-                time=datetime.now(),
-                event=MakeFlex(start_date=booking_date, desk_index=desk_index),
-            )
-        )
-        await interaction.response.send_message(f"Desk {desk} is now a flex desk from {date_str} onwards.")
+    async def makeflex(interaction: Interaction, start_date_str: str, desk: Range[int, 1]) -> None:
+        await eadk_bot.makeflex(CommandInfo.from_interaction(interaction), start_date_str, desk).send(interaction)
         database.save(database_path)
 
     @bot.command()
@@ -331,34 +148,10 @@ def setup_bot(database_path: Path, guilds: list[Snowflake]) -> Bot:
 
     @bot.tree.error
     async def on_error(interaction: Interaction, error: AppCommandError) -> None:
-        if isinstance(error, discord.app_commands.errors.MissingAnyRole) or isinstance(
-            error, discord.app_commands.errors.MissingRole
-        ):
-            await interaction.response.send_message("You do not have permission to run this command.", ephemeral=True)
-            return
-        if isinstance(error, discord.app_commands.errors.CheckFailure):
-            await interaction.response.send_message(
-                "This command can only be used in the office channel.", ephemeral=True
-            )
-            return
-        if isinstance(error, discord.app_commands.errors.TransformerError):
-            match error.__cause__:
-                case DateParseError(arg):
-                    await interaction.response.send_message(
-                        f"Date {arg} could not be parsed. "
-                        "Please use the format YYYY-MM-DD, 'today', 'tomorrow', or specify a weekday.",
-                        ephemeral=True,
-                    )
-                    return
-        if isinstance(error, discord.app_commands.errors.CommandInvokeError):
-            match error.__cause__:
-                case HandleEventError(_event, event_error):
-                    await interaction.response.send_message(
-                        event_error.message(lambda id: fmt.user(interaction, id)), ephemeral=True
-                    )
-                    return
-        else:
-            await interaction.response.send_message("INTERNAL ERROR HAS OCCURRED BEEP BOOP", ephemeral=True)
-            raise error
+        try:
+            await eadk_bot.handle_error(CommandInfo.from_interaction(interaction), error).send(interaction)
+        except Exception:
+            await Response(message="INTERNAL ERROR HAS OCCURRED BEEP BOOP", ephemeral=True).send(interaction)
+            raise
 
     return bot

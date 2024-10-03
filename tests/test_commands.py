2d6f220a043a2da1a8fe714080e91eb2e3c9fa39
Albert Garde <albertsgarde@gmail.com>
2024-10-03 15:30:51 +0200

    :white_check_mark: Split tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 
 import pytest
 
@@ -7,27 +7,37 @@ from eadk_discord.database import Database
 from eadk_discord.event import Event, SetNumDesks
 from eadk_discord.state import DateTooEarlyError
 
+NOW: datetime = datetime.fromisoformat("2024-09-14")  # Saturday
+TODAY: date = NOW.date()
 
-def test() -> None:
-    now = datetime.fromisoformat("2024-09-14")  # Saturday
-    today = now.date()
 
-    database = Database.initialize(now)
-    database.handle_event(Event(author=None, time=now, event=SetNumDesks(date=today, num_desks=6)))
+def setup() -> tuple[EADKBot, Database]:
+    database = Database.initialize(TODAY)
+    database.handle_event(Event(author=None, time=NOW, event=SetNumDesks(date=TODAY, num_desks=6)))
 
     bot = EADKBot(database)
 
+    return bot, database
+
+
+def test_book() -> None:
+    bot, database = setup()
+
+    response = bot.book(
+        CommandInfo(now=NOW, format_user=lambda user: str(user), author_id=1), "today", user_id=3, desk_arg=2
+    )
+    assert response.ephemeral is False
+    assert database.state.day(TODAY)[0].desk(0).booker is None
+    assert database.state.day(TODAY)[0].desk(1).booker == 3
+
+
+def test_book_too_early() -> None:
+    bot, _ = setup()
+
     with pytest.raises(DateTooEarlyError):
         bot.book(
-            CommandInfo(now=now, format_user=lambda user: str(user), author_id=1),
-            (today - timedelta(1)).isoformat(),
+            CommandInfo(now=NOW, format_user=lambda user: str(user), author_id=1),
+            (TODAY - timedelta(1)).isoformat(),
             user_id=0,
             desk_arg=1,
         )
-
-    response = bot.book(
-        CommandInfo(now=now, format_user=lambda user: str(user), author_id=1), today.isoformat(), user_id=3, desk_arg=2
-    )
-    assert response.ephemeral is False
-    assert database.state.day(today)[0].desk(0).booker is None
-    assert database.state.day(today)[0].desk(1).booker == 3

17f25c4adfee36d3d6e217ad25ee9eeb3ff13bdf
Albert Garde <albertsgarde@gmail.com>
2024-10-05 11:40:16 +0200

    :white_check_mark: Add test for `info` command

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -2,6 +2,7 @@ import pytest
 from conftest import NOW
 
 from eadk_discord.bot import CommandInfo, EADKBot
+from eadk_discord.database.event_errors import DateTooEarlyError
 from eadk_discord.dates import DateParseError
 
 
@@ -12,4 +13,14 @@ def test_date_invalid(bot: EADKBot) -> None:
             date_str="invalid",
             user_id=None,
             desk_num=None,
+        )
+
+
+def test_info(bot: EADKBot) -> None:
+    response = bot.info(CommandInfo(now=NOW, format_user=lambda user: str(user), author_id=1), date_str=None)
+    assert response.ephemeral is True
+
+    with pytest.raises(DateTooEarlyError):
+        response = bot.info(
+            CommandInfo(now=NOW, format_user=lambda user: str(user), author_id=1), date_str="2021-01-01"
         )

e51ca74a012eeb91e8d3cdafc4931347999ec8f5
Albert Garde <albertsgarde@gmail.com>
2024-10-16 16:33:29 +0200

    :art: Use `argparse` for argument parsing

--- eadk_discord/__main__.py
+++ eadk_discord/__main__.py
@@ -1,31 +1,30 @@
 # pragma: coverage exclude file
+import argparse
 import logging
 import os
-import sys
 import tomllib
 from pathlib import Path
 
 from eadk_discord import bot_setup
 
-
-def get_config_path(env_var_name: str) -> str:
-    match len(sys.argv):
-        case 1:
-            env_var_option: str | None = os.getenv(env_var_name)
-            if env_var_option is None:
-                raise ValueError(f"environment variable '{env_var_name}' is not set")
-            else:
-                return env_var_option
-        case 2:
-            return sys.argv[1]
-        case num_args:
-            raise ValueError(f"expected 0 or 1 argument, got {num_args-1}")
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    config_path = Path(get_config_path("EADK_DISCORD_CONFIG_PATH"))
+    parser = argparse.ArgumentParser(prog="EADK Discord Bot")
+    parser.add_argument("config_path")
+    args = parser.parse_args()
+
+    if args.config_path:
+        config_path = Path(args.config_path)
+    else:
+        match os.getenv("EADK_DISCORD_CONFIG"):
+            case None:
+                raise ValueError(
+                    "config path is provided neither as an "
+                    "argument nor as the environment variable 'EADK_DISCORD_CONFIG'"
+                )
+            case config_path_str:
+                config_path = Path(config_path_str)
 
     with open(config_path, "rb") as config_file:
         config = bot_setup.BotConfig.model_validate(tomllib.load(config_file))
